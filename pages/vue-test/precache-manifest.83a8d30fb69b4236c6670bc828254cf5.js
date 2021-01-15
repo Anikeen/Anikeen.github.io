ed268a159d53f894d4d35acdf05a905c3901616b
Anikeen <p.anikeen@gmail.com>
2021-01-15 14:58:37 +0200

    Add test work

---
+++ pages/vue-test/precache-manifest.83a8d30fb69b4236c6670bc828254cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6df307f80bd3a206bf",
+    "url": "/pages/vue-test/css/app.4671298c.css"
+  },
+  {
+    "revision": "9b381e166dab12dfd90979deb4080ec4",
+    "url": "/pages/vue-test/index.html"
+  },
+  {
+    "revision": "7b6df307f80bd3a206bf",
+    "url": "/pages/vue-test/js/app.9d18cc58.js"
+  },
+  {
+    "revision": "08cf150826cee3b34cfc",
+    "url": "/pages/vue-test/js/chunk-vendors.ba204bd9.js"
+  },
+  {
+    "revision": "5e778fbc6c0a0111fe8cf6a20d14e37e",
+    "url": "/pages/vue-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pages/vue-test/robots.txt"
+  }
+]);
